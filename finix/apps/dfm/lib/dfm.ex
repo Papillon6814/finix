915de11e100487ed401959f627ddcdc17dd771e9
Papillon6814 <so1rou.k@gmail.com>
2021-10-20 18:48:21 +0900

    feat: basis

--- finix/apps/dfm/lib/dfm.ex
+++ finix/apps/dfm/lib/dfm.ex
@@ -3,16 +3,102 @@ defmodule Dfm do
   Documentation for `Dfm`.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
-
-      iex> Dfm.hello()
-      :world
-
+  @script """
+  local curr = redis.call("GET", KEYS[1])
+  local next = redis.call("HGET", KEYS[2], curr)
+  if next then
+    redis.call("SET", KEYS[1], next)
+    return { next, true }
+  else
+    return { curr, false }
+  end
   """
-  def hello do
-    :world
+
+  @redis_host "localhost"
+  @redis_port 6379
+
+  defp conn() do
+    with {:ok, conn} <- Redix.start_link(host: @redis_host, port: @redis_port) do
+      conn
+    else
+      error -> raise "Failed to connect to #{@redis_host}:#{@redis_port} #{error}"
+    end
+  end
+
+  def flushall() do
+    conn = conn()
+    Redix.command(conn, ["FLUSHALL"])
+
+    Logger.info("Flushed all")
+  end
+
+  @doc """
+  Initializes state of automaton.
+  """
+  @spec initialize(String.t(), integer(), String.t()) :: Redix.Protocol.redis_value()
+  def initialize(key_name, db_index, initial_state) do
+    conn = conn()
+    name = name(key_name)
+
+    Redix.command!(conn, ["SELECT", db_index])
+    Redix.command!(conn, ["SET", name, initial_state, "NX"])
+  end
+
+  @spec name(String.t()) :: String.t()
+  defp name(key_name), do: "finite:#{key_name}"
+
+  @spec event_key(String.t(), String.t()) :: String.t()
+  defp event_key(key_name, event), do: "#{key_name}:#{event}"
+
+  @doc """
+  Defines how automaton changes the state.
+  """
+  @spec on(String.t(), integer(), String.t(), String.t(), String.t()) :: Redix.Protocol.redis_value()
+  def on(key_name, db_index, event, current_state, next_state) do
+    conn = conn()
+
+    Redix.command!(conn, ["SELECT", db_index])
+    Redix.command!(conn, ["HSET", event_key(key_name, event), current_state, next_state])
+  end
+
+  @doc """
+  Removes a pattern of state change.
+  """
+  @spec rm(String.t(), integer(), String.t()) :: Redix.Protocol.redis_value()
+  def rm(key_name, db_index, event) do
+    conn = conn()
+
+    Redix.command!(conn, ["SELECT", db_index])
+    Redix.command!(conn, ["HDEL", event_key(key_name, event)])
+  end
+
+  @doc """
+  Return current state.
+  """
+  @spec state(String.t(), integer()) :: Redix.Protocol.redis_value()
+  def state(key_name, db_index) do
+    conn = conn()
+
+    Redix.command!(conn, ["SELECT", db_index])
+    Redix.command!(conn, ["GET", name(key_name)])
+  end
+
+  @spec send_event(String.t(), integer(), String.t()) :: Redix.Protocol.redis_value()
+  defp send_event(key_name, db_index, event) do
+    conn = conn()
+
+    Redix.command!(conn, ["SELECT", db_index])
+    Redix.command!(conn, ["EVAL", @script, 2, name(key_name), event_key(key_name, event)])
+  end
+
+  @doc """
+  Triggers state change.
+  """
+  @spec trigger(String.t(), integer(), String.t()) :: Redix.Protocol.redis_value()
+  def trigger(key_name, db_index, event) do
+    result = send_event(key_name, db_index, event)
+    IO.inspect(result)
   end
 end
--- finix/apps/dfm/test/dfm_test.exs
+++ finix/apps/dfm/test/dfm_test.exs
@@ -2,7 +2,29 @@ defmodule DfmTest do
   use ExUnit.Case
   doctest Dfm
 
-  test "greets the world" do
-    assert Dfm.hello() == :world
+  describe "dfm" do
+    test "just works" do
+      Dfm.flushall()
+
+      key_name = "user1"
+      db_index = 15
+
+      state1 = "a"
+      state2 = "b"
+      state3 = "c"
+      trigger1 = "x"
+      trigger2 = "y"
+
+      Dfm.initialize(key_name, db_index, state1)
+      Dfm.on(key_name, db_index, trigger1, state1, state2)
+      Dfm.on(key_name, db_index, trigger1, state2, state3)
+      Dfm.on(key_name, db_index, trigger1, state3, state1)
+      Dfm.on(key_name, db_index, trigger2, state1, state3)
+
+      assert Dfm.state(key_name, db_index) == state1
+      Dfm.trigger(key_name, db_index, trigger1)
+      assert Dfm.state(key_name, db_index) == state2
+      Dfm.trigger(key_name, db_index, "t")
+    end
   end
 end
